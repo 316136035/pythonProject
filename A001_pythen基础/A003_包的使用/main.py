5657019fd99dd1781c78f9ef7b50762a5704a2d0
316136035 <316136035@qq.com>
2024-04-01 02:43:38 +0800

    A001

--- A001_pythen基础/A003_包的使用/main.py
+++ A001_pythen基础/A003_包的使用/main.py
@@ -1,15 +1,2 @@
-import sys
-
-# # 查找并移除旧路径
-# if 'e:\\开发代码笔记\\pythonProject\\A001_pythen基础\\A003_包的使用' in sys.path:
-#     sys.path.remove('e:\\开发代码笔记\\pythonProject\\A001_pythen基础\\A003_包的使用')
-
-# # 添加新路径
-# if 'e:\\开发代码笔记\\pythonProject' not in sys.path:
-#     sys.path.insert(0, 'e:\\开发代码笔记\\pythonProject')
-
-# # 验证修改后的sys.path
-print(sys.path)
-
-# from my_project import module
-# module()#调用包中的函数
+from my_project import module
+module.hello_world()
