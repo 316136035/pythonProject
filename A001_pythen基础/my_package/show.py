6957c143f546a12d3d212c82298a288657917038
316136035 <316136035@qq.com>
2024-04-01 01:25:24 +0800

    A001

--- A001_pythen基础/my_package/show.py
+++ A001_pythen基础/my_package/show.py
@@ -1,3 +1,10 @@
+auto=8888
 def print_python_list(*lst):
     for element in lst:
         print(element)
+    # 或者直接一次性打印整个列表：
+    # print(*lst)
+
+# 使用示例
+# my_list = [1, 2, 3, 4, 5]
+# print_python_list(my_list)
