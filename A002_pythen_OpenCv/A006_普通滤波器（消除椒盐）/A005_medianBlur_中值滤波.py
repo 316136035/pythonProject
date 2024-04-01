f2853f5728b6ec9e7ae85ece6c61d403fc484bd1
316136035 <316136035@qq.com>
2024-04-01 23:19:30 +0800

    A001

--- A002_pythen_OpenCv/A006_普通滤波器（消除椒盐）/A005_medianBlur_中值滤波.py
+++ A002_pythen_OpenCv/A006_普通滤波器（消除椒盐）/A005_medianBlur_中值滤波.py
@@ -8,7 +8,7 @@ img=cv2.imread('././images/salt_and_pepper.png')
 img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)#转换为灰度图
 # 对图片进行模糊处理,消除椒盐噪声特别有效，(图像,卷积大小)
 # 中值滤波. 参数：图像，核大小
-medianBlur=cv2.medianBlur(img,5)
+medianBlur=cv2.medianBlur(img,3)
 cv2.imshow('img',np.hstack([img,medianBlur]))
 cv2.waitKey(5000)
 
--- A002_pythen_OpenCv/A006_普通滤波器（消除椒盐）/A006_.bilateralFilter_双边滤波.py
+++ A002_pythen_OpenCv/A006_普通滤波器（消除椒盐）/A006_.bilateralFilter_双边滤波.py
@@ -8,7 +8,7 @@ img=cv2.imread('././images/salt_and_pepper.png')
 img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)#转换为灰度图
    # 更好地保留图像的边缘和细节，尤其适合去除噪声的同时保持边缘清晰。
     #（图像，滤波器直径必须为正奇数，颜色空间的标准差，坐标空间的标准差）
-bilateralFilter=cv2.bilateralFilter(img, 9, 75, 75)
+bilateralFilter=cv2.bilateralFilter(img,3,75, 75)
 cv2.imshow('img',np.hstack([img,bilateralFilter]))
 cv2.waitKey(5000)
 
