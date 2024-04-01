db4a8cce24d7d391ac140d276abb3c8686ee1750
316136035 <316136035@qq.com>
2024-04-01 20:20:24 +0800

    A001

--- A002_pythen_OpenCv/A007_高通滤波器（边缘检查）/A004_ Laplacian_拉普拉斯算子 copy.py
+++ A002_pythen_OpenCv/A007_高通滤波器（边缘检查）/A004_ Laplacian_拉普拉斯算子 copy.py
@@ -10,10 +10,11 @@ newImages = []
 # 遍历所有图片
 for image in images:
     # 更好地保留图像的边缘和细节，尤其适合去除噪声的同时保持边缘清晰。
-    #（图像，滤波器直径必须为正奇数，颜色空间的标准差，坐标空间的标准差）
-    newImage = cv2.bilateralFilter(image,d=3, sigmaColor=1,sigmaSpace=3)
-    newImage =cv2.Canny(newImage,100,200) #边缘检测改变阈值 数值越大
+    # （图像，滤波器直径必须为正奇数，颜色空间的标准差，坐标空间的标准差）
+    newImage = cv2.bilateralFilter(image, d=3, sigmaColor=1, sigmaSpace=3)
+    newImage = cv2.Canny(newImage, 20,50)  # 边缘检测改变阈值 数值越大检测到的边缘越少
     # 将处理后的图片添加到列表中
+
     newImages.append(newImage)
 # 显示处理后的图片
 imagesApi.show_Image_all(newImages)
