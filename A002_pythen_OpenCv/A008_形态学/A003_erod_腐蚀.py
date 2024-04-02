c148408cbf319a05bfbe3857fb6d68b0f2970272
316136035 <316136035@qq.com>
2024-04-02 17:22:53 +0800

    A001

---
+++ A002_pythen_OpenCv/A008_形态学/A003_erod_腐蚀.py
@@ -0,0 +1,18 @@
+
+from my_package import imagesApi  # 引入自定义包
+import cv2  # 导入cv2模块
+import numpy as np
+
+
+img=cv2.imread('././images/erod.png')
+image=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY) # 灰度图
+while True:
+  # 腐蚀 参数1：原图 2：核 3：迭代次数
+  erode=cv2.erode(image, np.ones((3, 3), np.uint8), iterations=1)
+  image= erode
+  cv2.imshow("erode", erode)
+  cv2.waitKey(1000)
+
+
+
+
