fe55c995063fa15e3e7122972fe0a4678f7bbb6b
316136035 <316136035@qq.com>
2024-04-02 16:42:35 +0800

    A001

--- A002_pythen_OpenCv/A008_形态学/A001_全局二值化_threshold.py
+++ A002_pythen_OpenCv/A008_形态学/A001_全局二值化_threshold.py
@@ -6,7 +6,8 @@ import numpy as np
 # 获取所有图片
 images = imagesApi.return_Image_all()  # 假设这是一个返回图片列表的函数
 newImages = []
-imagesApi.show_Image_all(images)
+
+
 
 # 遍历所有图片
 for image in images:
@@ -36,8 +37,9 @@ for image in images:
 # 不是单独使用，而是与其他阈值类型结合使用，例如 cv2.THRESH_BINARY + cv2.THRESH_OTSU。
 # Otsu's二值化方法会自动计算出一个最优的全局阈值，以最大程度地提高前景和背景像素之间的类间方差。
 # 在这种情况下，thresh 参数不再是你直接指定的阈值，而是程序会自动计算阈值。
-  # 二值化:灰度图像,阈值,最大值,阈值类型（请求查看上面）
-  _,newImage= cv2.threshold( image, 100, 255, cv2.THRESH_TRUNC)
+  # 二值化:灰度图像,阈值,阈值最大值,阈值类型（请求查看上面）
+  image=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY) # 灰度图
+  _,newImage= cv2.threshold( image, 100,175,cv2.THRESH_TRUNC)
   newImages.append(newImage)
 # 显示处理后的图片
 imagesApi.show_Image_all(newImages)
