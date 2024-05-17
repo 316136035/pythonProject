f55e140484854361e1a3f0a080ee4f5fcfbf9084
316136035 <316136035@qq.com>
2024-05-17 17:38:11 +0800

    A001

---
+++ A003_图形界面tkinter/A015_OptionMenu_下拉选择框.py
@@ -0,0 +1,50 @@
+import tkinter as tk
+from tkinter import StringVar
+
+
+# 定义 窗口类 继承自tk.Frame的类。这意味着Application类将拥有tk.Frame类的所有属性和方法  Frame是一个容器类
+class Application(tk.Frame):
+    # 构造器 # master: 窗口的父容器
+    def __init__(self, master=None):
+        super().__init__(
+            master, bg="#0066FF"
+        )  # super()函数用于调用tk.Frame父类的方法  把Frame容器放到master父容器
+        master.title("A010_grid_网格布局")  # 设置窗口标题
+        self.master = master  # 设置窗口父容器
+        self.center_window(800, 600)  # 调用窗口居中函数
+        self.create_widgets()  # 调用窗口组件函数
+        self.pack(
+            fill=tk.BOTH, expand=True
+        )  # 设置容器类布局 通过fill=tk.BOTH），而且当父容器的大小发生变化时，Frame也会随之扩大以继续填充父容器（通过expand=True）
+
+    # 定义一个函数，用于将窗口居中于屏幕 参数：root：窗口实例，width：窗口宽度，height：窗口高度
+    def center_window(self, width, height):
+        """将窗口居中于屏幕"""
+        screen_width = root.winfo_screenwidth()  # 获取屏幕宽度
+        screen_heigh = root.winfo_screenheight()  # 获取屏幕高度
+        x_coordinate = int((screen_width / 2) - (width / 2))  # 计算窗口的x坐标
+        y_coordinate = int((screen_heigh / 2) - (height / 2))  # 计算窗口的y坐标
+        self.master.geometry(
+            f"{width}x{height}+{x_coordinate}+{y_coordinate}"
+        )  # 设置窗口大小和位置
+
+    # 创建窗口组件
+    def create_widgets(self):
+        # 创建一个StringVar变量来存储选中的值
+        self.option_var = StringVar(root)
+        # 绑定选项改变事件到回调函数
+        self.option_var.trace("w", self.on_option_select)         
+        options = ["Option 1", "Option 2", "Option 3", "Option 4"]
+
+        option_menu=tk.OptionMenu(self,self.option_var,*options)   
+        option_menu.pack()
+        
+    def on_option_select(self,event):
+        """当选项被选中时的回调函数"""
+        selected_value = self.option_var.get()
+        print(f"Selected: {selected_value}")
+
+    
+root = tk.Tk()  # 创建主窗口
+app = Application(master=root)  # 创建Application对象
+app.mainloop()  # 进入消息循环
