ab1a9feaa33f8b48d348202d3d8bf885dc9545ef
316136035 <316136035@qq.com>
2024-06-24 14:54:34 +0800

    A001

--- A004_python进阶/A001_装饰器/A003_有参数的装饰器 copy.py
+++ A004_python进阶/A001_装饰器/A003_有参数的装饰器 copy.py
@@ -1,5 +1,6 @@
 import time
 
+#定义装饰器
 def decorator(func):
     def inner(*args, **kwargs):
         print("函数执行前")  # 自定义执行前的函数
@@ -10,10 +11,11 @@ def decorator(func):
         return result  # 返回原始函数的结果
     return inner  
 
-@decorator  # 装饰器同等于decorator=decorator(my_func)
+@decorator  # @decorator 装饰器同等于decorator=decorator(my_func)
+#被装饰的函数
 def my_func(*args, **kwargs):  # 给 *args 指定了一个形参名args，保持与装饰器内部调用一致
     time.sleep(1)
-    print("my_func")
+    print("被装饰的函数执行了")
     for i in args:
         print(i)
     return "函数执行完毕"  # 添加一个返回值以便观察装饰器是否正确返回结果
