4d9a229465aade608e2a4a6f121a8fc16f87f49c
316136035 <316136035@qq.com>
2024-06-28 01:21:23 +0800

    A001

--- A004_python进阶/A002_线程/A003.py
+++ A004_python进阶/A002_线程/A003.py
@@ -2,20 +2,27 @@ import threading
 import random
 import time
 
-threading.local()
+mylocal=threading.local()
+mylocal.name="主线程"
 class myThread(threading.Thread):
-    def __init__(self, threadID, name, counter):
+    def __init__(self):
         super().__init__()
         
     def run(self):
-        print("Starting " + self.name)
+        #获取当前线程并获取当前线程的属性
+        print("子线程前:",threading.current_thread(),mylocal.__dict__)
+        #设置当前线程的属性
+        mylocal.name=f"消息/隔离{threading.current_thread().name}"
+        #获取当前线程并获取当前线程的属性
+        print("子线程后:",threading.current_thread(),mylocal.__dict__)
 
 
 if __name__ == "__main__":
-    thread1 = myThread(1, "Thread-1", 1)
-    thread2 = myThread(2, "Thread-2", 2)
+    thread1 = myThread()
+    thread2 = myThread()
     thread1.start()
     thread2.start()
     thread1.join()
     thread2.join()
-    print("Exiting Main Thread")
+    #获取主线程的属性
+    print(mylocal.__dict__)
