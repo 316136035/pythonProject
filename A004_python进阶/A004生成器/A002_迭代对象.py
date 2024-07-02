fd0d812214ceda3f48d565c6cca2abb6709e5f80
316136035 <316136035@qq.com>
2024-07-02 17:46:32 +0800

    A001

--- A004_python进阶/A004生成器/A002_迭代对象.py
+++ A004_python进阶/A004生成器/A002_迭代对象.py
@@ -1,9 +1,8 @@
 from itertools import count # 迭代器
 from collections.abc import Iterable  # 判断对象是否为可迭代对象
 
+# 创建迭代器
 counter = count(10)
-
-
 
 print("查看类型:",type(counter))
  # 判断对象是否为可迭代对象 有__iter__方法和__next__方法 就是迭代器
@@ -24,12 +23,15 @@ class Person(object):
     return self.personal[item]
  
  
-p=Person(["a"]) # 创建对象
+p=Person(["A","B"]) # 创建对象
 
-print(dir(p)) # 查看对象属性
+# print(dir(p)) # 查看对象属性
 print(type(p)) # 查看类型
 p_iter = iter(p) # 获取对象迭代器
 print(type(p_iter)) # 查看类型
-print(dir(p_iter)) # 查看对象属性
+# print(dir(p_iter)) # 查看对象属性
 
+
+for i in p: # 迭代对象
+  print(i)
 
