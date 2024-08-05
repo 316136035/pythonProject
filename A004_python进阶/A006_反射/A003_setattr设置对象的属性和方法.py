6359fbb2b1517fb0aed81d705979480cdafae119
316136035 <316136035@qq.com>
2024-08-05 16:50:12 +0800

    A001

--- A004_python进阶/A006_反射/A003_setattr设置对象的属性和方法.py
+++ A004_python进阶/A006_反射/A003_setattr设置对象的属性和方法.py
@@ -1,16 +1,32 @@
 class Stu:
-  def __init__(self,name,age) -> None:
+  def __init__(self,name) -> None:
     self.name = name
-    self.age = age
-  
+
   def sleep(self):
     print(f'{self.name}正在睡觉')
 
-  def eat(self):
-    print(f'{self.name}正在吃饭')
+stu = Stu('张三')
 
-    
-stu = Stu('张三',18)
+
+def eat():
+  print('正在吃饭')
+
+keyinput=input('请输入属性名')
+valueinput=input('请输入属性值')
+if valueinput.isdigit(): #判断是否是数字
+  setattr(stu,keyinput,int(valueinput)) #设置对象的属性 value转成数字
+  print(stu.__dict__) #打印对象
+  
+funstr=input('请输入方法名')
+setattr(stu,funstr,eat) #设置对象的方法参数(对象,方法名,方法)
+stu.addeat() #调用对象的方法注意就是输入的方法名
+
+
+
+
+
+
+
 
 
 
