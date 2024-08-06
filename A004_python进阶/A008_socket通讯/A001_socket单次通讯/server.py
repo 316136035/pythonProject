05f836ef137d6a1030cf3225d39742a6ee7d4a3d
316136035 <316136035@qq.com>
2024-08-06 17:27:33 +0800

    A001

--- A004_python进阶/A008_socket通讯/A001_socket单次通讯/server.py
+++ A004_python进阶/A008_socket通讯/A001_socket单次通讯/server.py
@@ -1,23 +1,47 @@
 import socket
 import threading
 
-  
-  
-def server():
-  host = '0.0.0.0' #0.0.0.0 标识监听任意地址 
-  port = 8080 # 绑定端口8080..
-  #函数是Python的socket模块中的方法，用于创建一个套接字对象。参数指定使用IPv4地址族。参数指定使用面向连接的TCP协议
-  my_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-  while True:
+
+def handle_client(client_socket, addr):
+
     try:
-      my_server.bind((host, port)) #绑定端口
-      my_server.listen(5) #监听
-      print('服务器启动成功，监听端口：%s' % port)
-      break
+        # 接收数据长度信息
+        data_length_str = client_socket.recv(10).decode().strip()
+        data_length = int(data_length_str)
+
+        # 根据长度接收数据
+        total_received = 0
+        received_data = b'' # 初始化接收数据变量
+        while total_received < data_length: 
+            chunk = client_socket.recv(min(4096, data_length - total_received)) # 读取数据
+            if not chunk: # 如果没有接收到数据，则关闭连接
+                break
+            received_data += chunk # 将接收到的数据添加到接收数据变量中
+            total_received += len(chunk) # 更新已接收数据长度
+        print(received_data.decode())
+        
+
     except Exception as e:
-      print('服务器启动失败，端口：%s' % port)
-      port += 1
-      
-if __name__ == '__main__':
-  server()
+        print(f"Error: {e}")
+
+    finally:
+        # 关闭客户端套接字
+        client_socket.close()
+        
+
+def start_server():
+    host = '0.0.0.0'
+    port = 8080
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+        server_socket.bind((host, port))
+        server_socket.listen(5)
+
+        while True:
+            client_socket, addr = server_socket.accept()
+            client_thread = threading.Thread(target=handle_client, args=(client_socket, addr))
+            client_thread.start()
+
+if __name__ == "__main__":
+    start_server()
