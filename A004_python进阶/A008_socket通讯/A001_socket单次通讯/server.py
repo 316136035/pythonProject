c1cd7ac608fa9e097229e6fd6472e44d1f0c2285
316136035 <316136035@qq.com>
2024-08-06 17:01:29 +0800

    A001

--- A004_python进阶/A008_socket通讯/A001_socket单次通讯/server.py
+++ A004_python进阶/A008_socket通讯/A001_socket单次通讯/server.py
@@ -1,13 +1,23 @@
 import socket
 import threading
-#函数是Python的socket模块中的方法，用于创建一个套接字对象。参数指定使用IPv4地址族。参数指定使用面向连接的TCP协议
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#0.0.0.0 标识监听任意地址 绑定端口8080
-server.bind(('0.0.0.0', 8080))
 
-data,address= server.accept()  #等待客户端连接 阻塞直到有客户端连接
+  
+  
+def server():
+  host = '0.0.0.0' #0.0.0.0 标识监听任意地址 
+  port = 8080 # 绑定端口8080..
+  #函数是Python的socket模块中的方法，用于创建一个套接字对象。参数指定使用IPv4地址族。参数指定使用面向连接的TCP协议
+  my_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-print(data.recv(1024))
-  
-  
-  
+  while True:
+    try:
+      my_server.bind((host, port)) #绑定端口
+      my_server.listen(5) #监听
+      print('服务器启动成功，监听端口：%s' % port)
+      break
+    except Exception as e:
+      print('服务器启动失败，端口：%s' % port)
+      port += 1
+      
+if __name__ == '__main__':
+  server()
